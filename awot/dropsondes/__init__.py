5d44bb1b0894b0b8d023ac6e631d7277d271c68b
adlyons <andrew.d.lyons-1@ou.edu>
2015-04-02 12:52:15 -0500

    Dropsondes Try 2
    forgot to add the files to the last commit. testing to see if they
    work.

---
+++ awot/dropsondes/__init__.py
@@ -0,0 +1,14 @@
+"""
+awot - Airborne Weather Observations Toolkit
+================================================
+Probe Subpackage (:mod:'awot)
+================================================
+
+.. currentmodule:: awot
+
+.. autosummary::
+    :toctree: generated/
+
+"""
+
+__all__ = [s for s in dir() if not s.startswith('_')]
