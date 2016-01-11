8462cb30b71ee35935a5e66f3434eecf52a93d0f
adlyons <andrew.d.lyons-1@ou.edu>
2016-01-11 16:15:52 -0700

    change import structure for __init__

--- awot/dropsondes/__init__.py
+++ awot/dropsondes/__init__.py
@@ -10,5 +10,10 @@ Probe Subpackage (:mod:'awot)
     :toctree: generated/
 
 """
+from __future__ import absolute_import
+from thermocalcs import ThermoCalcs
+from shearcalcs import ShearCalcs
+from skew import SkewXTick
+
 
 __all__ = [s for s in dir() if not s.startswith('_')]
