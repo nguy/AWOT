0aae35dec24a1259325070fe6d7bb902a284e968
swnesbitt <snesbitt@illinois.edu>
2016-08-18 13:26:20 -0500

    Enhancements to kd-tree maching, including Barnes smoothing

--- awot/util/write_kmz.py
+++ awot/util/write_kmz.py
@@ -23,7 +23,7 @@ try:
     import simplekml
 except:
     raise ValueError("This module requires installation of simplekml...")
-    return
+#    return
 
 
 def write_track_kmz(awot, field, lat_name=None, lon_name=None,
